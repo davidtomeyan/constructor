e1f026031912c0618b7ba437b4b473e2f048afc4
tomeyandavid <david.tomeyan@cerberusholdings.org>
2025-07-27 22:00:14 +0200

    updates

--- src/futures/email.ts
+++ src/futures/email.ts
@@ -1,23 +1,29 @@
-import nodemailer from 'nodemailer'
+// import nodemailer from 'nodemailer'
 import { nodemailerAdapter } from '@payloadcms/email-nodemailer'
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  secure: process.env.NODE_ENV === "production",
-  auth: {
-    type: "OAuth2",
-    user: "tomeyandavid1@gmail.com",
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   secure: process.env.NODE_ENV === "production",
+//   auth: {
+//     type: "OAuth2",
+//     user: "tomeyandavid1@gmail.com",
+//     clientId: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+//   },
+// });
 
 export const emailFuture = nodemailerAdapter({
-  defaultFromAddress: 'tomeyandavid1@gmail.com',
-  defaultFromName: 'Tomeyan.ru',
-  transport:transporter,
-  // transportOptions: {
-  //
-  // },
+  defaultFromAddress: process.env.DEFAULT_FROM_ADDRESS!,
+  defaultFromName: process.env.DEFAULT_FROM_NAME!,
+  // transport:transporter,
+  transportOptions: {
+    secure:true,
+    host: process.env.SMTP_HOST,
+    port: 465,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  },
 })
--- src/globals/footer/social-media-block.tsx
+++ src/globals/footer/social-media-block.tsx
@@ -1,4 +1,5 @@
 import { ISocialMediaFooterBlock } from '@/payload-types'
+import NextLink from 'next/link'
 import { SocialIcons } from '@/components/social-icons'
 
 export function SocialMediaBlock (block:ISocialMediaFooterBlock){
@@ -6,14 +7,14 @@ export function SocialMediaBlock (block:ISocialMediaFooterBlock){
   return (
     <div className="flex flex-1 gap-6 flex-wrap justify-center ">
       {block.items.map((item, itemIndex) => (
-        <a
+        <NextLink
           target="_blank"
           rel="noopener noreferrer"
           href={item.url}
           key={`footer-block-item-${itemIndex}`}
         >
           <SocialIcons className="size-5" iconName={item.platformName} />
-        </a>
+        </NextLink>
       ))}
     </div>
   )
